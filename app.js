078aebfe33a4635c77529d8e346d1a1195a8b2d9
sherif <devsherif0@gmail.com>
2024-02-26 17:54:05 +0200

    add views and server files

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
-
 const PORT = 3000;
 
-app.use('/', require('./server/routes/main'))
 
+
+// Engine Template;
+app.use(expressLayouts);
+app.set('layouts', './layouts/main.ejs');
+app.set('view engine', 'ejs');
+
+// Adding the main routes;
+app.use('/', require('./server/routes/main'));
+
+// 404 Page;
 app.use((req, res) => {
-    res.render('404', {title : "Error! 404"})
+    res.render('404', {title : "Error! 404"});
 });
 
+// Add book Pages;
+app.get('/add-book', (req, res) => {
+    res.render('add-book', {title : 'Add-Book'});
+})
+
+// Server Connection;
 app.listen(PORT, () => {
     console.log("Connected to the Server");
 });

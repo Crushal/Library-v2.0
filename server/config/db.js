07a10985e83857930357743319e0158569bf1b94
crushal <devsherif0@gmail.com>
2024-02-28 20:24:58 +0200

    to tell connected to database

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const URI = process.env.dpURI;
 
-const dbConnect = () => mongoose.connect(URI);
+const dbConnect = () => mongoose.connect(URI)
+                                .then(() => {
+                                    console.log("database connected");
+                                })
+                                .catch(err => console.log(err))
 
 module.exports = dbConnect;
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const books = [{title : 'Harry potter', author : "JK Rawling", pages : 300, read : true}]
+    const books = [];
     res.render('index', {title : "Home", books});
 })
 

7808f29d034239507e6b041e89585d3baf49f74a
crushal <devsherif0@gmail.com>
2024-02-29 14:57:36 +0200

    add book model and post method

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../model/book');
 
-router.get('/', (req, res) => {
-    const books = [];
-    res.render('index', {title : "Home", books});
-})
+// URL Encoder;
+router.use(express.urlencoded({extended : true}));
+
+// Viewing data from database
+router.get('/', (req, res) => {    
+    Book.find()
+        .then((result) => {
+            res.render('index', {title : "Home", books : result});
+        })
+        .catch(err => console.log(err))
+});
+
+// Getting data from form;
+router.post('/', (req, res) => {
+    const book = new Book(req.body);
+    book.save()
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+});
+
 
 module.exports = router;
